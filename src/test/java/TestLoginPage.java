ed8aef1040735a37143fab5dd69c3e0ea0b5eae8
MariuszSolvd <mkaminski@solvd.com>
2025-04-07 16:00:30 +0200

    Refactor naming based on Andrey's feedback

--- src/test/java/TestLoginPage.java
+++ src/test/java/TestLoginPage.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.assertEquals;
 
-public class TestLoginPageBase extends AbstractTest {
+public class TestLoginPage extends AbstractTest {
 
     @Test
     public void shouldLogin() {
