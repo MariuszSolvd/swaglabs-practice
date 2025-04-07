962a8cb2a4a66753e513ea0e0fb6cecde98da02e
MariuszSolvd <mkaminski@solvd.com>
2025-04-07 14:09:12 +0200

    Implemented 1st test

--- src/test/java/TestLoginPage.java
+++ src/test/java/TestLoginPage.java
@@ -1,13 +1,17 @@
 import com.solvd.pages.base.LoginPage;
+import com.solvd.pages.base.ProductPage;
 import com.zebrunner.carina.core.AbstractTest;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
 
 public class TestLoginPage extends AbstractTest {
 
     @Test
     public void shouldLogin() {
         LoginPage loginPage = initPage(getDriver(), LoginPage.class);
-        loginPage.login("standard_user", "secret_sauce");
-        //todo:assert + new page
+        ProductPage productPage = loginPage.login("standard_user", "secret_sauce");
+        assertTrue(productPage.isPageOpened());
     }
 }
+
