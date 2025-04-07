2cdbbb5624773f70300a66b346b1f5473df4dfdf
MariuszSolvd <mkaminski@solvd.com>
2025-04-07 14:39:12 +0200

    Second test implemented

--- src/test/java/TestLoginPage.java
+++ src/test/java/TestLoginPage.java
@@ -4,6 +4,7 @@ import com.zebrunner.carina.core.AbstractTest;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
 
 public class TestLoginPage extends AbstractTest {
 
@@ -12,6 +13,14 @@ public class TestLoginPage extends AbstractTest {
         LoginPage loginPage = initPage(getDriver(), LoginPage.class);
         ProductPage productPage = loginPage.login("standard_user", "secret_sauce");
         assertTrue(productPage.isPageOpened());
+    }
+
+    @Test
+    public void shouldNotLoginWrongCredential() {
+        LoginPage loginPage = initPage(getDriver(), LoginPage.class);
+        loginPage.login("no_user", "no_password");
+        assertEquals(loginPage.getErrorMessage(),
+                "Username and password do not match any user in this service.");
     }
 }
 
