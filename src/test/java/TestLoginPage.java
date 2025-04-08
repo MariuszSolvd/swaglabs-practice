6bd3b9e795365a22256388e659d2aa842b140e52
MariuszSolvd <mkaminski@solvd.com>
2025-04-08 10:51:27 +0200

    Remove getDriver() from initPage

--- src/test/java/TestLoginPage.java
+++ src/test/java/TestLoginPage.java
@@ -11,14 +11,14 @@ public class TestLoginPage extends AbstractTest {
 
     @Test
     public void loginTest() {
-        LoginPageBase loginPageBase = initPage(getDriver(), LoginPageBase.class);
+        LoginPageBase loginPageBase = initPage(LoginPageBase.class);
         ProductPageBase productPage = loginPageBase.login(R.TESTDATA.get("standard_user"), R.TESTDATA.get("correct_password"));
         assertTrue(productPage.isPageOpened(),"Did not login to App");
     }
 
     @Test
     public void wrongCredentialTest() {
-        LoginPageBase loginPageBase = initPage(getDriver(), LoginPageBase.class);
+        LoginPageBase loginPageBase = initPage(LoginPageBase.class);
         loginPageBase.login("no_user", "no_password");
         assertEquals(loginPageBase.getErrorMessage(),
                 "Username and password do not match any user in this service.");
@@ -26,21 +26,21 @@ public class TestLoginPage extends AbstractTest {
 
     @Test
     public void missingUsernameTest() {
-        LoginPageBase loginPageBase = initPage(getDriver(), LoginPageBase.class);
+        LoginPageBase loginPageBase = initPage(LoginPageBase.class);
         loginPageBase.login("", R.TESTDATA.get("correct_password"));
         assertEquals(loginPageBase.getErrorMessage(), "Username is required");
     }
 
     @Test
     public void missingPasswordTest() {
-        LoginPageBase loginPageBase = initPage(getDriver(), LoginPageBase.class);
+        LoginPageBase loginPageBase = initPage(LoginPageBase.class);
         loginPageBase.login(R.TESTDATA.get("standard_user"), "");
         assertEquals(loginPageBase.getErrorMessage(), "Password is required");
     }
 
     @Test
     public void lockedUserTest() {
-        LoginPageBase loginPageBase = initPage(getDriver(), LoginPageBase.class);
+        LoginPageBase loginPageBase = initPage(LoginPageBase.class);
         loginPageBase.login(R.TESTDATA.get("locked_user"), R.TESTDATA.get("correct_password"));
         assertEquals(loginPageBase.getErrorMessage(), "Sorry, this user has been locked out.");
     }
