f44bada715408d2ecbf6080f1504282a1c4b18e6
MariuszSolvd <mkaminski@solvd.com>
2025-04-08 10:53:30 +0200

    Change name of LoginPageBase instances to loginPage

--- src/test/java/TestLoginPage.java
+++ src/test/java/TestLoginPage.java
@@ -11,38 +11,38 @@ public class TestLoginPage extends AbstractTest {
 
     @Test
     public void loginTest() {
-        LoginPageBase loginPageBase = initPage(LoginPageBase.class);
-        ProductPageBase productPage = loginPageBase.login(R.TESTDATA.get("standard_user"), R.TESTDATA.get("correct_password"));
+        LoginPageBase loginPage = initPage(LoginPageBase.class);
+        ProductPageBase productPage = loginPage.login(R.TESTDATA.get("standard_user"), R.TESTDATA.get("correct_password"));
         assertTrue(productPage.isPageOpened(),"Did not login to App");
     }
 
     @Test
     public void wrongCredentialTest() {
-        LoginPageBase loginPageBase = initPage(LoginPageBase.class);
-        loginPageBase.login("no_user", "no_password");
-        assertEquals(loginPageBase.getErrorMessage(),
+        LoginPageBase loginPage = initPage(LoginPageBase.class);
+        loginPage.login("no_user", "no_password");
+        assertEquals(loginPage.getErrorMessage(),
                 "Username and password do not match any user in this service.");
     }
 
     @Test
     public void missingUsernameTest() {
-        LoginPageBase loginPageBase = initPage(LoginPageBase.class);
-        loginPageBase.login("", R.TESTDATA.get("correct_password"));
-        assertEquals(loginPageBase.getErrorMessage(), "Username is required");
+        LoginPageBase loginPage = initPage(LoginPageBase.class);
+        loginPage.login("", R.TESTDATA.get("correct_password"));
+        assertEquals(loginPage.getErrorMessage(), "Username is required");
     }
 
     @Test
     public void missingPasswordTest() {
-        LoginPageBase loginPageBase = initPage(LoginPageBase.class);
-        loginPageBase.login(R.TESTDATA.get("standard_user"), "");
-        assertEquals(loginPageBase.getErrorMessage(), "Password is required");
+        LoginPageBase loginPage = initPage(LoginPageBase.class);
+        loginPage.login(R.TESTDATA.get("standard_user"), "");
+        assertEquals(loginPage.getErrorMessage(), "Password is required");
     }
 
     @Test
     public void lockedUserTest() {
-        LoginPageBase loginPageBase = initPage(LoginPageBase.class);
-        loginPageBase.login(R.TESTDATA.get("locked_user"), R.TESTDATA.get("correct_password"));
-        assertEquals(loginPageBase.getErrorMessage(), "Sorry, this user has been locked out.");
+        LoginPageBase loginPage = initPage(LoginPageBase.class);
+        loginPage.login(R.TESTDATA.get("locked_user"), R.TESTDATA.get("correct_password"));
+        assertEquals(loginPage.getErrorMessage(), "Sorry, this user has been locked out.");
     }
 }
 
