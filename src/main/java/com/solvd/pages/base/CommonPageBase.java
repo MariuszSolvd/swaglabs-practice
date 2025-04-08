ea6e67cd2b1d7c6f1021f1f82e3cfb9e5ac5e3cf
MariuszSolvd <mkaminski@solvd.com>
2025-04-08 16:04:50 +0200

    Added IMobileUtils for custom tap in ProductPage

--- src/main/java/com/solvd/pages/base/CommonPageBase.java
+++ src/main/java/com/solvd/pages/base/CommonPageBase.java
@@ -1,9 +1,10 @@
 package com.solvd.pages.base;
 
+import com.zebrunner.carina.utils.mobile.IMobileUtils;
 import com.zebrunner.carina.webdriver.gui.AbstractPage;
 import org.openqa.selenium.WebDriver;
 
-public abstract class CommonPageBase extends AbstractPage {
+public abstract class CommonPageBase extends AbstractPage implements IMobileUtils {
 
     protected static final int IS_OPENED_DEFAULT_TIMEOUT = 10;
 
