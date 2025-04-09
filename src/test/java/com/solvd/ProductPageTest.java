0fd686d7c202750dfa035b6dbc7b909303c7d811
MariuszSolvd <mkaminski@solvd.com>
2025-04-09 13:25:45 +0200

    Implemented 1st test for product page

--- src/test/java/com/solvd/ProductPageTest.java
+++ src/test/java/com/solvd/ProductPageTest.java
@@ -1,11 +1,31 @@
 package com.solvd;
 
+import com.solvd.entity.Product;
+import com.solvd.pages.base.ProductDetailPageBase;
+import com.solvd.pages.base.ProductPageBase;
+import com.solvd.pages.base.elements.ProductBase;
 import org.testng.annotations.Test;
 
-public class ProductPageTest {
+import java.util.List;
 
-    @Test
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
+
+public class ProductPageTest extends BaseMobileTest {
+
+    @Test(description = "Check if product information are the same before and after click on product")
     public void productsDetailsTest() {
-
+        ProductPageBase productPage = loginService.loginToApp();
+        assertTrue(productPage.isOpened(), "Product page is not opened");
+        List<? extends ProductBase> products = productPage.getProducts();
+        for (ProductBase product : products) {
+            Product productFromProductsPage = new Product(product.getImage(), product.getTitle(), product.getPrice());
+            ProductDetailPageBase productDetailPage = product.clickOnProduct();
+            Product productFromDetailPage = new Product(productDetailPage.getImage(),
+                    productFromProductsPage.title(), productFromProductsPage.price());
+            assertEquals(productFromDetailPage, productFromProductsPage, "Products are not the same!");
+            productDetailPage.clickBackToProductsButton();
+        }
     }
+
 }
