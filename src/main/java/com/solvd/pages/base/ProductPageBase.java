316f7679ce562bc2e9cdd0f3933bc28d864d2663
MariuszSolvd <mkaminski@solvd.com>
2025-04-09 14:08:21 +0200

    Added SortButton

--- src/main/java/com/solvd/pages/base/ProductPageBase.java
+++ src/main/java/com/solvd/pages/base/ProductPageBase.java
@@ -1,6 +1,7 @@
 package com.solvd.pages.base;
 
 import com.solvd.pages.base.elements.ProductBase;
+import com.solvd.pages.base.elements.SortBase;
 import org.openqa.selenium.WebDriver;
 
 import java.util.List;
@@ -10,6 +11,8 @@ public abstract class ProductPageBase extends CommonPageBase {
     public ProductPageBase(WebDriver driver) {
         super(driver);
     }
+
+    public abstract SortBase clickOnSortButton();
 
     public abstract List<? extends ProductBase> getProducts();
 }
--- src/main/java/com/solvd/pages/ios/ProductPage.java
+++ src/main/java/com/solvd/pages/ios/ProductPage.java
@@ -1,6 +1,7 @@
 package com.solvd.pages.ios;
 
 import com.solvd.pages.base.ProductPageBase;
+import com.solvd.pages.base.elements.SortBase;
 import com.solvd.pages.ios.elements.Product;
 import com.zebrunner.carina.utils.factory.DeviceType;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
@@ -16,12 +17,16 @@ public class ProductPage extends ProductPageBase {
     @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeStaticText[`name == \"PRODUCTS\"`]")
     private ExtendedWebElement pageTitle;
 
+    @ExtendedFindBy(iosPredicate = "name == \"test-Modal Selector Button\"")
+    private ExtendedWebElement sortButton;
+
     @ExtendedFindBy(iosPredicate = "name == \"test-Item\"")
     private List<Product> products;
 
     public ProductPage(WebDriver driver) {
         super(driver);
     }
+
 
     @Override
     public boolean isOpened() {
@@ -31,6 +36,12 @@ public class ProductPage extends ProductPageBase {
     @Override
     public boolean isOpened(int timeout) {
         return pageTitle.isElementPresent(timeout);
+    }
+
+    @Override
+    public SortBase clickOnSortButton() {
+        sortButton.click();
+        return initPage(SortBase.class);
     }
 
     @Override
