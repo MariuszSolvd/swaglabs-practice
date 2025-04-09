5a32e4689be96e2952ae4e5604e45036f3ffb9f8
MariuszSolvd <mkaminski@solvd.com>
2025-04-09 14:15:52 +0200

    Added Sort

---
+++ src/main/java/com/solvd/pages/ios/elements/Sort.java
@@ -0,0 +1,53 @@
+package com.solvd.pages.ios.elements;
+
+import com.solvd.pages.base.ProductPageBase;
+import com.solvd.pages.base.elements.SortBase;
+import com.zebrunner.carina.utils.factory.DeviceType;
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
+import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
+import org.openqa.selenium.WebDriver;
+
+@DeviceType(pageType = DeviceType.Type.IOS_PHONE, parentClass = SortBase.class)
+public class Sort extends SortBase {
+
+    @ExtendedFindBy(iosPredicate = "name == \"Name (A to Z)\"")
+    private ExtendedWebElement sortNameAToZButton;
+
+    @ExtendedFindBy(iosPredicate = "name == \"Name (Z to A)\"")
+    private ExtendedWebElement sortNameZToAButton;
+
+    @ExtendedFindBy(iosPredicate = "name == \"Price (low to high)\"")
+    private ExtendedWebElement sortPriceLowToHighButton;
+
+    @ExtendedFindBy(iosPredicate = "name == \"Price (high to low)\"")
+    private ExtendedWebElement sortPriceHighToLowButton;
+
+    public Sort(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public ProductPageBase clickSortNameAToZ() {
+        return clickOnSortButton(sortNameAToZButton);
+    }
+
+    @Override
+    public ProductPageBase clickSortNameZToA() {
+        return clickOnSortButton(sortNameZToAButton);
+    }
+
+    @Override
+    public ProductPageBase clickSortPriceLowToHigh() {
+        return clickOnSortButton(sortPriceLowToHighButton);
+    }
+
+    @Override
+    public ProductPageBase clickSortPriceHighToLow() {
+        return clickOnSortButton(sortPriceHighToLowButton);
+    }
+
+    private ProductPageBase clickOnSortButton(ExtendedWebElement element) {
+        element.click();
+        return initPage(ProductPageBase.class);
+    }
+}
