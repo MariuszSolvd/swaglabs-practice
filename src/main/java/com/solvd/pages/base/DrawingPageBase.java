db1f6bafcf1cb8e276cce10ff868ee11829779cf
MariuszSolvd <mkaminski@solvd.com>
2025-04-11 15:44:22 +0200

    Fix DrawingPageBase Add isDrawingScreenVisible()

--- src/main/java/com/solvd/pages/base/DrawingPageBase.java
+++ src/main/java/com/solvd/pages/base/DrawingPageBase.java
@@ -2,9 +2,11 @@ package com.solvd.pages.base;
 
 import org.openqa.selenium.WebDriver;
 
-public class DrawingPageBase extends CommonPageBase{
+public abstract class DrawingPageBase extends CommonPageBase{
 
     public DrawingPageBase(WebDriver driver) {
         super(driver);
     }
+
+    public abstract boolean isDrawingScreenVisible();
 }
